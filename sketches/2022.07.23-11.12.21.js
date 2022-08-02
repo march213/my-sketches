9cd58289b388a69e0fa586492faaa3b24462d73b
march213 <jane.molodetskaya@gmail.com>
2022-08-01 22:03:06 -0400

    added randon colors and shadows

--- sketches/2022.07.23-11.12.21.js
+++ sketches/2022.07.23-11.12.21.js
@@ -1,6 +1,8 @@
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
+const Color = require('canvas-sketch-util/color');
+const risoColors = require('riso-colors');
 
 const settings = {
   dimensions: [1080, 1080],
@@ -8,11 +10,13 @@ const settings = {
 };
 
 const sketch = ({ width, height }) => {
-  let x, y, w, h;
+  let x, y, w, h, fill, stroke;
   const num = 20;
   const angleDeg = -30;
 
   const rects = [];
+  const rectColors = [random.pick(risoColors), random.pick(risoColors), random.pick(risoColors)];
+  const bgColor = random.pick(risoColors).hex;
 
   for (let i = 0; i < num; i++) {
     x = random.range(0, width);
@@ -20,19 +24,35 @@ const sketch = ({ width, height }) => {
     w = random.range(200, 600);
     h = random.range(40, 200);
 
-    rects.push({ x, y, w, h });
+    fill = random.pick(rectColors).hex;
+    stroke = random.pick(rectColors).hex;
+
+    rects.push({ x, y, w, h, fill, stroke });
   }
 
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = bgColor;
     context.fillRect(0, 0, width, height);
 
-    rects.forEach(({ x, y, w, h }) => {
+    rects.forEach(({ x, y, w, h, fill, stroke }) => {
+      let shadowColor;
       context.save();
       context.translate(x, y);
-      context.strokeStyle = 'blue';
+      context.strokeStyle = stroke;
+      context.fillStyle = fill;
+      context.lineWidth = 10;
 
       drawSkewedRect({ context, w, h, degrees: angleDeg });
+      shadowColor = Color.offsetHSL(fill, 0, 0, -20);
+      shadowColor.rgba[3] = 0.5;
+
+      context.shadowColor = Color.style(shadowColor.rgba);
+      context.shadowOffsetX = -10;
+      context.shadowOffsetY = 20;
+      context.fill();
+
+      context.shadowColor = null;
+      context.stroke();
 
       context.restore();
     });
@@ -54,7 +74,6 @@ const drawSkewedRect = ({ context, w = 600, h = 600, degrees = -45 }) => {
   context.lineTo(rx, ry + h);
   context.lineTo(0, h);
   context.closePath();
-  context.stroke();
 
   context.restore();
 };
