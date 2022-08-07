faac8cf298e7ab7878aa0e31107c1990925ef2d3
march213 <jane.molodetskaya@gmail.com>
2022-08-06 21:10:41 -0400

    setup audio analyser, context and data

--- sketches/2022.08.06-20.40.40.js
+++ sketches/2022.08.06-20.40.40.js
@@ -2,25 +2,45 @@ const canvasSketch = require("canvas-sketch");
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true,
 };
 
 let audio;
+let audioContext, audioData, sourceNode, analyserNode;
 
 const sketch = () => {
-  audio = document.createElement("audio");
-  audio.src = "audio/song.mp3";
-
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    if (!audioContext) return;
+
+    analyserNode.getFloatFrequencyData(audioData);
   };
 };
 
 const addListeners = () => {
   window.addEventListener("mouseup", () => {
+    if (!audioContext) createAudio();
+
     if (audio.paused) audio.play();
     else audio.pause();
   });
+};
+
+const createAudio = () => {
+  audio = document.createElement("audio");
+  audio.src = "audio/song.mp3";
+
+  audioContext = new AudioContext();
+
+  sourceNode = audioContext.createMediaElementSource(audio);
+  sourceNode.connect(audioContext.destination);
+
+  analyserNode = audioContext.createAnalyser();
+  sourceNode.connect(analyserNode);
+
+  audioData = new Float32Array(analyserNode.frequencyBinCount);
 };
 
 addListeners();
